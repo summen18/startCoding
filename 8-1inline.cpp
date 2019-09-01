d5082da1e9bb91fb8e39ebcee7c98289490670a4
summen18 <summen18@163.com>
2019-09-01 16:02:20 +0800

    "finished"

--- 8-1inline.cpp
+++ 8-1inline.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+inline double square(double x){return x*x;}
+
 int main()
 {
-    
+    double a,b;
+    double c = 13.0;
+
+    a = square(5.0);
+    b = square(4.5+7.5);
+
+    cout<<"a: "<<a<<" b;"<<b<<endl;
+
+    return 0;
+
 }
